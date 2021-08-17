baff3e4f4c01abe6f88d48eaae35a538136e9276
Kishore <kishore.sai37@gmail.com>
2021-08-17 21:20:39 +0530

    added logic

--- scripts/script.js
+++ scripts/script.js
@@ -6,12 +6,42 @@ const btnAnalyse = document.getElementById('btn-analyse');
 const result = document.getElementById('result');
 
 // Functions
-const showMessage = function (message) {
+const showMessage = function (message, color) {
   result.style.display = 'block';
+  result.style.color = color;
   result.textContent = message;
+  investedPrice.value = null;
+  shareCount.value = null;
+  finalPrice.value = null;
 };
 
-const shareAnalyser = function () {};
+const shareAnalyser = function (e) {
+  e.preventDefault();
+  const investedSharePrice = investedPrice.value;
+  const numberOfShares = shareCount.value;
+  const finalSharePrice = finalPrice.value;
+  if (investedSharePrice && numberOfShares && finalSharePrice) {
+    if (investedSharePrice < finalSharePrice) {
+      showMessage(
+        `You have invested ${numberOfShares} at ₹${investedSharePrice} now the final price is ₹${finalSharePrice} & you have made a profit of ₹${
+          (finalSharePrice - investedSharePrice) * numberOfShares
+        }`,
+        'green'
+      );
+    } else if (investedSharePrice > finalSharePrice) {
+      showMessage(
+        `You have invested ${numberOfShares} shared at ₹${investedSharePrice} now the final price is ₹${finalSharePrice} & you have made a loss of ₹${
+          (investedSharePrice - finalSharePrice) * numberOfShares
+        }`,
+        'red'
+      );
+    } else {
+      showMessage('You have no profit no loss', 'black');
+    }
+  } else {
+    showMessage('Enter all the values', 'red');
+  }
+};
 
 // Event Listeners
-btnCheck.addEventListener('click', shareAnalyser);
+btnAnalyse.addEventListener('click', shareAnalyser);
